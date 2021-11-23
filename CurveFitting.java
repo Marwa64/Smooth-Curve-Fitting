c49a292c89626f8fc923b3e89eb16ececd106ca3
Marwa Omar <farabi.marwa@gmail.com>
2021-11-23 13:34:23 +0200

    Add files via upload

---
+++ CurveFitting.java
@@ -0,0 +1,209 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+
+
+// This will store the x and y coordinates of each point
+class Pair {
+	 private float x, y;
+	
+	 public Pair() {
+		 x = 0;
+		 y = 0;
+	 }
+	 public Pair(double _x, double _y) {
+	     x   = (float) (_x);
+	     y = (float)(_y);
+	 }
+	
+	 public void setX(float _x) {  x = _x; }
+	 public void setY(float _y) { y = _y; }
+	 public float getX()   { return x; }
+	 public float getY() { return y; }
+}
+
+
+public class CurveFitting {
+
+	private ArrayList<ArrayList<Float>> People;
+	ArrayList<Pair> Points;
+	private int Degree;
+	private static int Population;
+	private static int MaximumGeneration;
+	
+	public CurveFitting(ArrayList<Pair> points, int degree, int population, int maximumGeneration) {
+		Points = points;
+		Degree = degree;
+		Population = population;
+		MaximumGeneration = maximumGeneration;
+
+		People = new ArrayList<ArrayList<Float>>();
+	}
+	
+	public void findBestIndividual() {
+		generatePeople();
+		
+		//go forward till reach generation limit
+		for (int i = 0; i < MaximumGeneration; i++) {
+			ArrayList<ArrayList<Float>> parents = getParents();
+			ArrayList<ArrayList<Float>> crossedover = crossover(parents.get(0), parents.get(1));
+			mutate(crossedover, i);
+		}
+		getBestChild(People);
+	}
+	
+	private void generatePeople() {
+		ArrayList<Float> coefficients;
+		
+		for (int i = 0; i < Population; i++) {
+			coefficients = new ArrayList<Float>();
+			
+			for (int j = 0; j < Degree + 1; j++) {
+				Random r = new Random();
+				float num =  -10 + r.nextFloat() * (10 - (-10));
+				coefficients.add(num);
+			}
+			People.add(coefficients);
+		}
+	}
+	
+	private void getBestChild(ArrayList<ArrayList<Float>> Persons) {
+		double bestFitness = getFitness(Persons.get(0));
+		ArrayList<Float> bestChild = null;
+		
+		for (int i = 1; i < Persons.size(); i++) {
+			double newFitness = getFitness(Persons.get(i));
+			if (newFitness <= bestFitness) {
+				bestChild = Persons.get(i);
+				bestFitness = newFitness;
+			}
+		}
+	    try {
+	        FileWriter myWriter = new FileWriter("Solution.txt");
+			for (int x = 0; x < bestChild.size(); x++) {
+				System.out.print(bestChild.get(x) + ", ");
+				myWriter.write(bestChild.get(x) + ", ");
+			}
+	        myWriter.write("Error = " + bestFitness);
+	        System.out.println("Error = " + bestFitness);
+	        myWriter.close();
+	      } catch (IOException e) {
+	        System.out.println("An error occurred.");
+	        e.printStackTrace();
+	      }
+
+	}
+	
+	private double getFitness(ArrayList<Float> Chromosome) {
+		double total, fitness = 0;
+
+		for (int i = 0; i < Points.size(); i++) {
+			total = 0;
+			for (int j = 0; j < Chromosome.size(); j++) {
+				total += Chromosome.get(j) * Math.pow(Points.get(i).getX(), j);
+			}
+			total -= Points.get(i).getY();
+			fitness += Math.pow(total, 2);
+		}
+
+		return fitness / Points.size();
+	}
+	
+	private ArrayList<ArrayList<Float>> getParents() {
+		ArrayList<ArrayList<Float>> Parents = new ArrayList<ArrayList<Float>>();
+		ArrayList<Pair> cumulativeFitness = new ArrayList<Pair>();
+		float totalFitness = 0;
+		Pair newPair;
+		
+		for (int i = 0; i < People.size(); i++) {
+			newPair = new Pair();
+			newPair.setX(totalFitness);
+			totalFitness += getFitness(People.get(i));
+			newPair.setY(totalFitness);
+			cumulativeFitness.add(newPair);
+		}
+		
+		Random rand = new Random();
+		boolean parentFound = false;
+		// Get Parent 1
+		do {
+			float r1 =  rand.nextFloat() * totalFitness;
+			for (int i = 0; i < People.size(); i++) {
+				if (r1 >= cumulativeFitness.get(i).getX() && r1 < cumulativeFitness.get(i).getY()) {
+					Parents.add(People.get(i));
+					totalFitness -= getFitness(People.get(i));
+					parentFound = true;
+					People.remove(i);
+					break;
+				}
+			}
+		} while (!parentFound);
+		
+		parentFound = false;
+		// Get Parent 2
+		do {
+			float r2 =  rand.nextFloat() * totalFitness;
+			for (int i = 0; i < People.size(); i++) {
+				if (r2 >= cumulativeFitness.get(i).getX() && r2 < cumulativeFitness.get(i).getY()) {
+					Parents.add(People.get(i));
+					People.remove(i);
+					parentFound = true;
+					break;
+				}
+			
+			}
+		} while (!parentFound);
+		
+		return Parents;
+	}
+	
+	private ArrayList<ArrayList<Float>> crossover(ArrayList<Float> individual1, ArrayList<Float> individual2) {
+		ArrayList<ArrayList<Float>> crossedOver = new ArrayList<ArrayList<Float>>();
+		ArrayList<Float> offspring1 = new ArrayList<Float>();
+		ArrayList<Float> offspring2 = new ArrayList<Float>();
+		
+		double random = Math.random() * (individual1.size() - 1);
+		double random2 = Math.random() * (individual1.size() - 1);
+		for (int i = 0; i < individual1.size(); i++) {
+			if (random >= i || random2 <= i) {
+				offspring1.add(individual1.get(i));
+				offspring2.add(individual2.get(i));
+			}
+			else {
+				offspring1.add(individual2.get(i));
+				offspring2.add(individual1.get(i));
+			}		
+		}
+		crossedOver.add(offspring1);
+		crossedOver.add(offspring2);
+		return crossedOver;
+	}
+	
+	private void mutate(ArrayList<ArrayList<Float>> crossedOver, int generation) {
+		ArrayList<Float> mutated;
+		for (int i = 0; i < crossedOver.size(); i++) {
+			mutated = new ArrayList<Float>();
+			for (int j = 0; j < crossedOver.get(i).size(); j++) {
+				double random = Math.random();
+				if (random <= crossedOver.get(i).get(j)) {
+					double lowerBound = crossedOver.get(i).get(j) -  (-10);
+					double upperBound = 10 - crossedOver.get(i).get(j);
+					double y;
+					if (Math.random() <= 0.5) {
+						y = lowerBound;
+					} else {
+						y = upperBound;
+					}
+					double power = 1 - (generation / MaximumGeneration);
+					float result = (float) (y * (1 - Math.pow(random, power)));
+					float newGene = crossedOver.get(i).get(j) - result;
+					mutated.add(newGene);
+				} else {
+					mutated.add(crossedOver.get(i).get(j));
+				}
+			}
+			People.add(mutated);
+		}
+	}
+}
---
+++ Main.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Main {
+	
+	/*public static void PrintAnswer(String Answer, ArrayList<Pair> weights_and_values, int Case) {
+		int chosenItemsCount = 0;
+		int totalValuePerCase = 0;
+		
+		// Answer consists of string of zeros and ones
+		// 0 means the item is not being selected, whereas 1 means the item is being selected
+		// count the number of the chosen items as well as their values
+		for (int i = 0; i < Answer.length(); i++) if (Answer.charAt(i) == '1') {
+			chosenItemsCount++;
+			totalValuePerCase += weights_and_values.get(i).value();
+		}
+		
+		System.out.println("Case# " + Case + ": " +totalValuePerCase);
+		System.out.println(chosenItemsCount);
+		for (int i = 0; i < Answer.length(); i++) {
+			if (Answer.charAt(i) == '1')
+				System.out.println(weights_and_values.get(i).weight() + " " + weights_and_values.get(i).value());
+		}
+	}*/
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// Open the file
+		FileInputStream fstream = new FileInputStream("input-2.txt");
+		BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+
+		String strLine;
+
+		//Read File Line By Line
+		int Case = 0;
+		while ((strLine = br.readLine()) != null)   {
+			int numberOfTestCases = Integer.parseInt(strLine);
+			while(numberOfTestCases != 0) {
+				ArrayList<Pair> points = new ArrayList<>();
+				
+				//read the number of items and polynomial degree
+				strLine = br.readLine();
+				String[] points_degree = strLine.split(" ");
+				int numberOfPoints = Integer.parseInt(points_degree[0]);
+				int degree =  Integer.parseInt(points_degree[1]);
+				
+				//get the pairs of x and y of each point
+				for (int i = 0; i < numberOfPoints; i++) {
+					strLine = br.readLine();
+					String[] coordinates = strLine.split(" ");
+					float x = Float.parseFloat(coordinates[0]);
+					float y = Float.parseFloat(coordinates[1]);
+					Pair pair = new Pair(x, y);
+					points.add(pair);
+				}
+								
+				CurveFitting curveFitting = new CurveFitting(points, degree, 200 * numberOfPoints, 50);
+				curveFitting.findBestIndividual();
+				numberOfTestCases--;
+			}
+		}
+		//Close the input stream
+		fstream.close();
+	}
+}
